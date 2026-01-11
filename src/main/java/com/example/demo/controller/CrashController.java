0d62fb9e181e588ed999137f27167ffa12e27f8f
TkWarrior <talibulhaque369@gmail.com>
2026-01-11 14:58:41 +0530

    added crash simulator controller

---
+++ src/main/java/com/example/demo/controller/CrashController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@RequestMapping("/crash")
+public class CrashController {
+
+    @GetMapping()
+    public String HelloController(){
+        return "Hello frontend";
+    }
+
+    // 1. Arithmetic Exception (Divide by zero)
+    @GetMapping("/divide")
+    public int CrashDivide(){
+        int x = 5 ;
+        int y = 0 ;
+        return x/y ; // arithematic exception
+    }
+
+    // 2. Null Pointer Exception
+    @GetMapping("/null")
+    public String CrashNull(){
+        String name = null ;
+        return name.toLowerCase(); // null pointer exception
+    }
+
+    // 3. Simulated Database Failure
+    @GetMapping("/crash/db")
+    public String crashDb() {
+        throw new RuntimeException("Database connection timeout");  //  fake DB failure
+    }
+
+    @GetMapping("/logs")
+    public String getLogs() throws IOException {
+        Path logFile = Path.of("logs/app.log");
+        if (!Files.exists(logFile)) {
+            return "Log file not found";
+        }
+        return Files.readString(logFile);
+    }
+
+}
